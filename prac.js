7d96ece53a7e2d4e70fe1011e18a1c3c01392794
Neha Thakur <tneha6260@gmail.com>
2024-12-24 09:14:16 +0545

    added a github practice page

---
+++ prac.js
@@ -0,0 +1,8 @@
+// Q1 . destructure the array into name age and car and log
+// 'ram of age 21 owns bmw'
+const user = ['ram', 21, 'bmw']
+
+// console.log(user[0] + 'of age' + user[1]+ 'owns'+ user[2])
+
+const [name , age , car] = user;
+console.log(`${name} of age ${age} owns ${car}`);
