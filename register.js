94b49900ed26bc6c6562aa2cf169a2a5cefcdc9f
sushimchaudhary <sushimchaudhary1@gmail.com>
2024-12-25 12:10:24 +0545

    changed Register form

--- register.js
+++ register.js
@@ -1,6 +1,7 @@
 <div>
   <input placeholder="Enter the name" />
+  <input type ="number" placeholder="Enter your number"/>
   <input type="text" placeholder="Enter the Email" />
   <input type="password" placeholder="Enter the password" />
   <button>Register</button>
-</div>;
+</div>
